671a099445ea0cbf5d6afed1c69dff76511d5e83
edu <eduardoblazquez34@gmail.com>
2024-02-16 13:17:46 +0100

    test: testing AgendaContacto methods

--- src/testing/TestingAgendaContacto.java
+++ src/testing/TestingAgendaContacto.java
@@ -1,14 +1,21 @@
 package testing;
 
+import modelo.dao.AgendaContacto;
 import modelo.javabean.Contacto;
 
 public class TestingAgendaContacto {
+    private static AgendaContacto agc1;
+    private static AgendaContacto agc2;
+    private static AgendaContacto agc3;
     private static Contacto contacto1;
     private static Contacto contacto2;
     private static Contacto contacto3;
     private static Contacto contacto4;
 
     static{
+        agc1=new AgendaContacto("Juanan");
+        agc1=new AgendaContacto("Sonia");
+        agc1=new AgendaContacto("Mario");
         contacto1=new Contacto("Sonia","García Manzano","639010345","sonig@yahoo.es","HP");
         contacto2=new Contacto("Edu","Blázquez García","694467818","eduardoblazquez34@gmail.com","PwC");
         contacto3=new Contacto("Juanjo","Pedrosa","613198217","juanjo@gmail.com","Telefónica");
@@ -16,6 +23,8 @@ public class TestingAgendaContacto {
     }
 
     public static void main(String[] args) {
+        //Testing Contacto.java
+
         System.out.println(contacto1.getEmail());
         contacto1.setEmail("abelardo@yahoo.es");
         System.out.println(contacto1.getEmail());
@@ -24,5 +33,12 @@ public class TestingAgendaContacto {
         System.out.println(contacto3.getApellidos());
         System.out.println(contacto4.getTelefono());
         System.out.println(contacto4.getNombre());
+
+        //Testing AgendaContacto.java
+        agc1.altaContacto(contacto1);
+        System.out.println(agc1.mostrarTodo());
+        System.out.println(agc1.buscarUno("Juan"));
+        System.out.println(agc1.buscarEmail("674591217"));
+        System.out.println(agc1.buscarTelefono("juanjo@gmail.com"));
     }
 }
